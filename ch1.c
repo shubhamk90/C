88e2e7aade0bfa7b0cb4ecd5fcd6226913258c6a
Shubham <sharmashubham52y@gmail.com>
2023-08-29 18:03:35 +0530

    Initial

---
+++ ch1.c
@@ -0,0 +1,98 @@
+// CH 1 = Variables,Constants And Keywords
+
+
+// (1)....
+/*
+ # include <stdio.h>
+int main(){
+    int a=4;                                                    
+    float b=5.5;                                                    // %d for integers
+    char c ='A';                                                    // %f for float
+    int d = 6;                                                      // % c for character 
+    int e = 50+50;                                                        
+    printf("The value of a is %d \n",a);                            //    \n is used for new line.
+    printf("The value of b is %f \n",b);
+    printf("The value of c is %c\n",c); 
+    printf("sum of and d is %d \n",a+d);
+    printf("The value of e is %d",e);
+    return 0; 
+}      
+*/
+
+
+
+
+/* AT A TIME EKK CODE HE RUN KRDE ESLIYE BACKIYAN NU 
+COMMENT OUT KITA*/
+
+
+
+
+
+
+// (2)........
+
+// Write a Program to add two integers.
+
+/*
+
+# include <stdio.h>
+ int main(){
+     int a=5;
+     int b=5;
+     printf("The value of a+b is %d",a+b);
+     return 0;
+ }   
+
+*/
+
+
+
+// (3).......
+
+// Take input from the user.
+
+/*
+
+# include <stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of a\n");
+    scanf("%d",&a);
+
+    printf("Enter the value of b\n");
+    scanf("%d",&b);
+
+    printf("Sum of a and b is %d",a+b);
+    return 0;
+}   
+
+*/
+
+// (4).........
+
+// Print character in this way.
+
+/*
+
+# include <stdio.h>
+int main(){
+	char a;
+	char b;
+	char c;
+	printf("Enter the char a \n");
+	scanf("%c",&a);
+
+	printf("Enter the char b \n");
+	scanf("\n%c",&b);
+
+	printf("Enter the char c \n");
+	scanf("\n%c",&c);
+
+	return 0;
+} 
+*/
+
+
+
+
